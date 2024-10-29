bea8897432995e9fea2ce5809373fd8dde2e588a
OlgaV0539 <olv-personal@mail.ru>
2024-10-29 11:41:59 +0700

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,9 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+zp = zip(first, second)
+first_result = (abs(len(f)-len(s)) for f, s in zp if len(f) != len(s))
+print(list(first_result))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(second_result))
